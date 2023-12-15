fe1ecb4ff4639b06c939334459525fb1b0b4d99f
Gunwo Yoon <glove2275@pusan.ac.kr>
2023-12-15 15:16:41 +0900

    argocd cli install

--- CICD/ArgoCD/install_argocd.sh
+++ CICD/ArgoCD/install_argocd.sh
@@ -6,3 +6,9 @@ kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/s
 
 # expose argocd service using LoadBalancer
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+# download argocd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64 --force
+
