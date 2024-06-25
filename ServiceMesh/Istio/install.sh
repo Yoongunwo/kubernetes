0d92b014254567bd1c3c92d79d87af9028f66763
Gunwo Yoon <glove2275@pusan.ac.kr>
2024-06-25 12:54:16 +0900

    rbac

--- ServiceMesh/Istio/install.sh
+++ ServiceMesh/Istio/install.sh
@@ -1,4 +1,4 @@
-# https://istio.io/latest/docs/setup/getting-started/
+# ref. https://istio.io/latest/docs/setup/getting-started/
 
 # Install Istioctl
 curl -L https://istio.io/downloadIstio | sh -
@@ -22,3 +22,54 @@ kubectl label namespace default istio-injection=enabled
 
 # deploy the sample application
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+
+# confirm the application is running
+kubectl exec "$(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}')" -c ratings -- curl -sS productpage:9080/productpage | grep -o "<title>.*</title>"
+
+# create a gateway for the application
+kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
+
+
+## 이후는 ref 참조
+
+kubectl apply -f - <<EOF
+apiVersion: security.istio.io/v1beta1
+kind: PeerAuthentication
+metadata:
+  name: "default"
+  namespace: "istio-system"
+spec:
+  mtls:
+    mode: STRICT
+EOF
+
+
+kubectl apply -f - <<EOF
+apiVersion: security.istio.io/v1
+kind: AuthorizationPolicy
+metadata:
+  name: allow-nothing
+  namespace: default
+spec:
+  {}
+EOF
+
+
+kubectl apply -f - <<EOF
+apiVersion: security.istio.io/v1
+kind: AuthorizationPolicy
+metadata:
+  name: "productpage-viewer"
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: productpage
+  action: ALLOW
+  rules:
+  - to:
+    - operation:
+        methods: ["GET"]
+EOF
+
+
