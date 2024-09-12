c7a2b53e7d51a665ee9ca78244bfc8cc338a4a98
Gunwo Yoon <glove2275@pusan.ac.kr>
2024-09-12 21:10:04 +0900

    edit argocd

--- CICD/ArgoCD/install_argocd.sh
+++ CICD/ArgoCD/install_argocd.sh
@@ -12,3 +12,7 @@ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/lat
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64 --force
 
+
+# default password for argocd
+kubectl -n argocd get secret argocd-initial-admin-secret \
+          -o jsonpath="{.data.password}" | base64 -d; echo
