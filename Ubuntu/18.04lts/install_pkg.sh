bd6da95b37e86ccfeb1533abe50122319bb61980
Gunwo Yoon <glove2275@pusan.ac.kr>
2026-03-19 23:06:42 +0900

    fix

--- Ubuntu/18.04lts/install_pkg.sh
+++ Ubuntu/18.04lts/install_pkg.sh
@@ -7,14 +7,14 @@ sudo apt-get install -y vim git
 # Install Docker
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo systemctl enable --now docker
 
 # Install Kubernetes
-sudo apt-get install -y kubelet kubeadm kubectl --allow-unauthenticated
-sudo install -o root -g root -m 0755 kubectl /usr/bin/kubectl
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo systemctl enable --now kubelet
 
--- Ubuntu/18.04lts/work_nodes.sh
+++ Ubuntu/18.04lts/work_nodes.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 # unknown service runtime.v1alpha2.RuntimeService error troubleshooting
-sudo sed -i '/^disabled_plugins = \["cri"\]/s/^/#/' /etc/containerd/config.toml
+# sudo sed -i '/^disabled_plugins = \["cri"\]/s/^/#/' /etc/containerd/config.toml
+# sudo systemctl restart containerd
+
+sudo containerd config default | tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
 
+kubeadm join 192.168.122.2:6443 --token 123456.1234567890123456 \
+        --discovery-token-ca-cert-hash sha256:47135b2d0215477128a6cf53f9e1e23407033c118c153364aa933bb04664cf2c
 
-# config for work_nodes only 
+# 워커 노드 설정
 sudo kubeadm join --token 123456.1234567890123456 \
-        --discovery-token-unsafe-skip-ca-verification 192.168.2.10:6443
+  --discovery-token-unsafe-skip-ca-verification 192.168.122.2:6443
